afbae52d512d2232ef5ba191c78f7cfd46a18010
schordas <schordas@google.com>
2022-10-11 15:11:06 -0700

    initial starter code
    Change-Id: Ic67a8bd19aecd866b08a013f99b4c916ee02ec56

--- app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
+++ app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
@@ -16,58 +16,32 @@
 
 package com.example.dessertrelease.ui
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.dessertrelease.DessertReleaseApplication
 import com.example.dessertrelease.R
-import com.example.dessertrelease.data.PreferencesRepository
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 /*
  * View model of Dessert Release components
  */
-class DessertReleaseViewModel(
-    private val userPreferencesRepository: PreferencesRepository
-) : ViewModel() {
+class DessertReleaseViewModel() : ViewModel() {
     // UI states access for various [DessertReleaseUiState]
-    val uiState: StateFlow<DessertReleaseUiState> =
-        userPreferencesRepository.isLinearLayoutPreference.map { isLinearLayout ->
-            DessertReleaseUiState(isLinearLayout)
-        }.stateIn(
-            scope = viewModelScope,
-            // Flow is set to emits value for when app is on the foreground
-            // 5 seconds stop delay is added to ensure it flows continuously
-            // for cases such as configuration change
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = DessertReleaseUiState()
-        )
+    val uiState: StateFlow<DessertReleaseUiState> = MutableStateFlow(DessertReleaseUiState())
 
     /*
      * [selectLayout] change the layout and icons accordingly and
      * save the selection in DataStore through [userPreferencesRepository]
      */
     fun selectLayout(isLinearLayout: Boolean) {
-        viewModelScope.launch {
-            userPreferencesRepository.saveLayoutPreference(isLinearLayout)
-        }
     }
 
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                val application = (this[APPLICATION_KEY] as DessertReleaseApplication)
-                val userPreferencesRepository = application.container.preferencesRepository
-                DessertReleaseViewModel(userPreferencesRepository)
+                DessertReleaseViewModel()
             }
         }
     }
